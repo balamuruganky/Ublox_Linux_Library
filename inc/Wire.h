1a692b59a5da2f63158b2408bc598c7f2d01f560
StevenMedusa <stevenvanderveeke@gmail.com>
2021-03-15 17:03:47 +0000

    added working i2c implementation

--- inc/Wire.h
+++ inc/Wire.h
@@ -3,7 +3,9 @@
 
 #include <fcntl.h>
 #include <linux/i2c-dev.h>
+#include <linux/i2c.h>
 #include <sys/ioctl.h>
+#include <cstdio>
 
 #include "Common.h"
 
@@ -15,15 +17,14 @@ public:
 
   bool init() {
     char filename[20] = I2C_DEV;
-    //snprintf(filename, sizeof(filename), "/dev/i2c-%u", bus_);
-
+    
     if ((i2c_fd = open(filename, O_RDWR)) < 0) {
       printf("I2C: Failed to open bus '%s': %i-%s",
                 filename, errno, strerror(errno));
       return false;
     }
 
-    printf("I2C: Successfully initialized with default address [%s 0x%02x]", filename, m_address);
+    printf("I2C: Successfully initialized with default address [%s 0x%02x]\n", filename, m_address);
     return true;
   }
 
@@ -34,32 +35,102 @@ public:
                 errno, strerror(errno));
       return false;
     }
+    return true;
   }
-  uint8_t endTransmission(void) { return 0; }
-  uint8_t endTransmission(bool stop) { return 0; }
+  uint8_t endTransmission(void) {  
+   if (m_sendBufPos > 0) {
+     if ( ::write(i2c_fd, m_sendBuf, m_sendBufPos) != m_sendBufPos) {
+            printf("Failed to write to the i2c bus.\n");
+            return -1;
+      }
+   }
 
-  bool available() {
-    return (read(m_recvdByte));
+    return 0; 
   }
+
+
+  uint8_t endTransmission(bool stop) { return endTransmission(); }
+  bool available() { return (m_readBufPos < m_quantity); }
+  uint8_t read() {return m_readBuf[m_readBufPos++];}
 
   size_t write(uint8_t byte) {
-    m_IsAvailable = false;
-    return ::write(i2c_fd, (void *)&byte, sizeof(byte));
+    if (byte == 0xFD) {get_available_bytes(); return m_sendBufPos;}
+
+    m_sendBuf[m_sendBufPos] = byte;
+    return m_sendBufPos++; 
   }
+
 
   size_t write(uint8_t *byte, size_t num_of_bytes) {
-    m_IsAvailable = false;
-    return ::write(i2c_fd, byte, num_of_bytes);
+    size_t res; 
+    for (size_t i = 0; i < num_of_bytes; i++){
+      res = write(byte[i]);
+    }
+    return res; 
   }
 
-  uint8_t read() {
-    return m_recvdByte;
-  }
+  
+  
 
+
+
+
+
+uint8_t get_available_bytes() {
+  // Dedicated function to read the available bytes register
+  // Should be done with a single read-and-then-write operation, otherwise no valid length is returned
+
+    
+    struct i2c_rdwr_ioctl_data packets;
+    struct i2c_msg messages[2];
+
+    unsigned char outBuf = 0xFD;
+    messages[0].addr  = m_address;
+    messages[0].flags = 0;
+    messages[0].len   = sizeof(outBuf);
+    messages[0].buf   = &outBuf;
+
+    /* The data will get returned in this structure */
+    messages[1].addr  = m_address;
+    messages[1].flags = I2C_M_RD;
+    messages[1].len   = 2; // registers 0xFD and 0xFE
+    messages[1].buf   = m_readBuf;
+
+    /* Send the request to the kernel and get the result back */
+    packets.msgs      = messages;
+    packets.nmsgs     = 2;
+    if(ioctl(i2c_fd, I2C_RDWR, &packets) < 0) {
+        perror("Unable to send data2");
+        return 1;
+    }
+
+    m_quantity = 2;
+    m_readBufPos = 0;
+    l_IsAvailable = true;
+
+    return 0;
+}
+
+
+  
   uint8_t requestFrom(uint8_t address, uint8_t quantity) {
+    if (l_IsAvailable) {
+      // in the previous command we have read the lengt buffer, no need to redo this:
+      l_IsAvailable = false;
+      return m_quantity;
+    }
+         
+
+    if (::read(i2c_fd, m_readBuf, quantity) != quantity) {
+            printf("Failed to read from the i2c bus.\n");
+            return 0;
+    }
+    
+
     m_address = address;
     m_quantity = quantity;
-    return (read(quantity));
+    m_readBufPos = 0;
+    return quantity;
   }
 
 private:
@@ -69,6 +140,14 @@ private:
   uint8_t m_recvdByte;
   uint8_t m_quantity;
   int i2c_fd;
+
+  unsigned char m_sendBuf[60];
+  uint8_t m_sendBufPos = 0;
+
+  unsigned char m_readBuf[60];
+  uint8_t m_readBufPos = 0;
+
+  bool l_IsAvailable = false;
 
   size_t read(uint8_t byte) {
     int iRxLen = 0;
